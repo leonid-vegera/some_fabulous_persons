455da70dabc679eb6f444261b263d9e3bf025b76
Leonid Vegera <leonid.vegera@gmail.com>
2023-10-04 20:52:05 +0300

    Card is done

--- App.js
+++ App.js
@@ -1,14 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, SafeAreaView, Platform, ScrollView, ImageBackground, StatusBar } from 'react-native';
 import { PersonCard } from './components/PersonCard';
 
 export default function App() {
+  const personData = {
+    name: "Andriy Bal`",
+    image: require('./assets/bal.jpg'),
+    position: 'Defender',
+    performanceYears: ['1981', '1990'],
+    championYears: ['1981', '1985', '1986', '1990'],
+    cupYears: ['1982', '1985', '1987', '1990'],
+    scores: 11,
+  };
+
   return (
-    <SafeAreaView>
-      <View style={styles.container}>
-        <PersonCard />
-      </View>
-    </SafeAreaView>
+    <View style={{ flex: 1}}>
+      <StatusBar backgroundColor='#0000cd' barStyle='light-content' />
+      <SafeAreaView style={styles.container}>
+        {/*<View style={styles.container}>*/}
+        <ImageBackground style={styles.bgImage} source={{uri: 'https://7arts.com.ua/content/images/20/1500x1500l80nn0/nastennyy-gerb-fk-dinamo-kiev-iz-dereva-76320059365919.webp'}}>
+          <ScrollView>
+            <PersonCard {...personData}/>
+            <PersonCard {...personData}/>
+            {/*<PersonCard />*/}
+            {/*<PersonCard />*/}
+          </ScrollView>
+
+        </ImageBackground>
+        {/*</View>*/}
+      </SafeAreaView>
+    </View>
   );
 }
 
@@ -16,7 +37,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#d2c8c8',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: Platform.OS === 'ios' ? 30 : 0
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
+  bgImage: {
+    // paddingTop: Platform.OS === 'android' ? -30 : 0,
+    flex: 1,
+  }
 });
--- components/PersonCard.jsx
+++ components/PersonCard.jsx
@@ -1,20 +1,128 @@
-import { View, Image, Text } from 'react-native';
-import image from '../assets/dynamo_romb.jpg';
+import { View, Image, Text, Platform, StyleSheet, useWindowDimensions, ImageBackground } from 'react-native';
+import ballImage from '../assets/ball.png';
 
-export const PersonCard = () => {
-  const image = require('../assets/dynamo_romb.jpg')
+export const PersonCard = ({ name, image, position, championYears, cupYears, scores, performanceYears }) => {
+  const windowWidth = useWindowDimensions().width;
+  const ballImage = require('../assets/ball.png');
   return (
-    <View>
-      <View>
-        <Text>Person Card</Text>
-        <Image source={image} />
+    <View
+      style={[styles.card,
+        {marginHorizontal: windowWidth > 500 && 120,
+          paddingHorizontal: windowWidth > 500 && 60}
+        ]}
+    >
+      <View style={styles.nameContainer}>
+        <Text style={styles.name}>{name}</Text>
+        <Text style={styles.position}>{position}</Text>
+      </View>
+
+      <Image
+        source={image}
+        style={styles.image}
+        resizeMode='contain'
+        accessibilityLabel={`${name} player`}
+      />
+
+      <View style={styles.scores}>
+        <Image
+          source={ballImage}
+          style={[styles.scoresImage, {right: windowWidth > 500 ? 120 : 40}]}
+          resizeMode='contain'
+        />
+        <View style={[styles.scoresTextWrapper, {right: windowWidth > 500 ? 130 : 50}]}>
+          <Text style={styles.scoresText}>{scores}</Text>
+        </View>
+      </View>
+
+      <View style={styles.descriptionContainer}>
+        <Text style={styles.description}>Years of performance: {performanceYears.join(' - ')}</Text>
+      </View>
+      <View style={styles.descriptionContainer}>
+        <Text style={styles.description}>🏅Champion years: {championYears.join(', ')}</Text>
+      </View>
+      <View style={styles.descriptionContainer}>
+        <Text style={styles.description}>🏆Cup winner years: {cupYears.join(', ')}</Text>
       </View>
     </View>
   )
 }
 
-const styles = {
-  container: {
-
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: 'white',
+    borderRadius: 16,
+    borderWidth: 2,
+    margin: 16,
+    padding: 20,
+    ...Platform.select({
+      ios: {
+        shadowColor: '#333',
+        shadowOffset: {
+          width: 6,
+          height: 6,
+        },
+        shadowOpacity: 0.3,
+        shadowRadius: 4
+      },
+      android: {
+        elevation: 5,
+        shadowColor: '#333',
+      }
+    })
+  },
+  nameContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'baseline',
+    marginBottom: 25,
+  },
+  name: {
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+  position: {
+    fontSize: 22,
+  },
+  image: {
+    width: '100%',
+    height: 350,
+    marginBottom: 25
+  },
+  scores: {
+    position: 'absolute',
+    top: 550,
+    right: -5,
+  },
+  scoresImage: {
+    position: 'absolute',
+    right: 40,
+    bottom: 110,
+    width: 90,
+    height: 90,
+    borderRadius: 50,
+    borderColor: 'navy',
+    borderWidth: 2,
+  },
+  scoresTextWrapper: {
+    position: 'absolute',
+    bottom: 130,
+    right: 50,
+    width: 70,
+    alignItems: 'center',
+  },
+  scoresText: {
+    fontSize: 40,
+    fontWeight: 'bold',
+    color: 'white',
+    textDecorationColor: 'yellow',
+    textShadowColor: '#000000',
+    textShadowRadius: 10,
+  },
+  descriptionContainer: {
+    marginBottom: 5
+  },
+  description: {
+    fontSize: 16,
+    fontWeight: 'bold',
   }
-}
+})
